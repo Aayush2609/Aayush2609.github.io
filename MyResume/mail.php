6ac7b0058752e7971205ae1275e3e71f85dcb0e8
Aayush2609 <aadeep2609@gmail.com>
2022-05-29 03:59:36 +0530

    done

---
+++ MyResume/mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "aayush.deep.contact@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
